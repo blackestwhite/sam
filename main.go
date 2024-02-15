37fcf0f0f7b66bf0b655c146ae2311d6d1a49c3c
blackestwhite <pesaregoal@gmail.com>
2024-02-15 14:45:44 +0330

    feat: Add suggestCommit function to suggest commit messages based on git diff output

--- main.go
+++ main.go
@@ -12,6 +12,22 @@ import (
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: sam <command>")
+		return
+	}
+
+	command := os.Args[1]
+
+	switch command {
+	case "commit":
+		suggestCommit()
+	default:
+		fmt.Println("Invalid command. Available commands: commit")
+	}
+}
+
+func suggestCommit() {
 	usr, err := user.Current()
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -19,24 +35,6 @@ func main() {
 	}
 
 	configFilePath := filepath.Join(usr.HomeDir, ".samrc")
-
-	_, err = os.Stat(configFilePath)
-	if os.IsNotExist(err) {
-		fmt.Print("Enter your OpenAI API key: ")
-		var apiKey string
-		fmt.Scanln(&apiKey)
-
-		err := os.WriteFile(configFilePath, []byte(apiKey), 0600)
-		if err != nil {
-			fmt.Println("Error writing to configuration file:", err)
-			return
-		}
-
-		fmt.Println("API key saved successfully.")
-	} else if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
 
 	apiKeyBytes, err := os.ReadFile(configFilePath)
 	if err != nil {
@@ -62,11 +60,11 @@ func main() {
 		Stream: true,
 		Model:  "gpt-3.5-turbo",
 		Messages: []gopenai.Message{
-			{
+			gopenai.Message{
 				Role:    "system",
 				Content: "you are a AI in a cli tool. you should suggest commit message based on output of git diff with the following format: 'label: commit message' label should be from common labels like: fix, feat, chore, refactor and ..., and also don't be verbose your response should be a one line response that should be used directly in cli tool.",
 			},
-			{
+			gopenai.Message{
 				Role:    "user",
 				Content: gitDiffOutput,
 			},
